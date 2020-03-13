2d679ba38736d2111b238972d61829c2bc333825
nemanjagajic <nemanja.gajicru96@gmail.com>
2020-03-13 19:40:25 +0100

    protected route implemented

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -13,11 +13,12 @@ const Login = ({ classes }) => {
     try {
       const idToken = googleUser.getAuthResponse().id_token
       const client = new GraphQLClient('http://localhost:4000/graphql', {
-        headers: {authorization: idToken}
+        headers: { authorization: idToken }
       })
       const { me } = await client.request(ME_QUERY)
       console.log(me)
-      dispatch({type: 'LOGIN_USER', payload: me})
+      dispatch({ type: 'LOGIN_USER', payload: me })
+      dispatch({ type: 'IS_LOGGED_IN', payload: googleUser.isSignedIn() })
     } catch(err) {
       console.log(() => console.log(err))
     }
--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import reducer from './reducer'
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import * as serviceWorker from "./serviceWorker";
+import ProtectedRoute from './ProtectedRoute'
 
 const Root = () => {
   const initialState = useContext(Context)
@@ -18,7 +19,7 @@ const Root = () => {
     <Router>
       <Context.Provider value={{ state, dispatch }}>
         <Switch>
-          <Route exact path="/" component={App} />
+          <ProtectedRoute exact path="/" component={App} />
           <Route path="/login" component={Splash} />
         </Switch>
       </Context.Provider>
